462e74e52c3be6963decff9304b7a36a79fe1cab
shalastra <szymon.halastra@gmail.com>
2019-09-14 22:15:48 +0200

    Rearrange Coffee model

--- src/main/java/io/shalastra/models/Coffee.java
+++ src/main/java/io/shalastra/models/Coffee.java
@@ -1,8 +1,26 @@
 package io.shalastra.models;
 
-public interface Coffee {
+import lombok.Getter;
+import lombok.ToString;
 
-    String getName();
-    int getRequiredGrainsAmount();
-    int getRequiredWaterAmount();
+@Getter
+@ToString
+public class Coffee {
+
+    private final String name;
+    private final int requiredGrainsAmount;
+    private final int requiredWaterAmount;
+
+    private int requiredMilkAmount = 0;
+
+    public Coffee(String name, int requiredGrainsAmount, int requiredWaterAmount) {
+        this.name = name;
+        this.requiredGrainsAmount = requiredGrainsAmount;
+        this.requiredWaterAmount = requiredWaterAmount;
+    }
+
+    public Coffee(String name, int requiredGrainsAmount, int requiredWaterAmount, int requiredMilkAmount) {
+        this(name, requiredGrainsAmount, requiredWaterAmount);
+        this.requiredMilkAmount = requiredMilkAmount;
+    }
 }
