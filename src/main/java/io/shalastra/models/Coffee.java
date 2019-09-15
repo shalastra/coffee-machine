58f267cbf8b78b576cb77cf4b749d2581615d8a7
shalastra <szymon.halastra@gmail.com>
2019-09-15 10:21:35 +0200

    Extract a recipe to separate class

--- src/main/java/io/shalastra/models/Coffee.java
+++ src/main/java/io/shalastra/models/Coffee.java
@@ -1,26 +1,14 @@
 package io.shalastra.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class Coffee {
 
     private final String name;
-    private final int requiredGrainsAmount;
-    private final int requiredWaterAmount;
-
-    private int requiredMilkAmount = 0;
-
-    public Coffee(String name, int requiredGrainsAmount, int requiredWaterAmount) {
-        this.name = name;
-        this.requiredGrainsAmount = requiredGrainsAmount;
-        this.requiredWaterAmount = requiredWaterAmount;
-    }
-
-    public Coffee(String name, int requiredGrainsAmount, int requiredWaterAmount, int requiredMilkAmount) {
-        this(name, requiredGrainsAmount, requiredWaterAmount);
-        this.requiredMilkAmount = requiredMilkAmount;
-    }
+    private final Recipe recipe;
 }
---
+++ src/main/java/io/shalastra/models/Recipe.java
@@ -0,0 +1,22 @@
+package io.shalastra.models;
+
+import lombok.Getter;
+
+@Getter
+public class Recipe {
+
+    private final int requiredGrainsAmount;
+    private final int requiredWaterAmount;
+
+    private int requiredMilkAmount = 0;
+
+    public Recipe(int requiredGrainsAmount, int requiredWaterAmount) {
+        this.requiredGrainsAmount = requiredGrainsAmount;
+        this.requiredWaterAmount = requiredWaterAmount;
+    }
+
+    public Recipe(int requiredGrainsAmount, int requiredWaterAmount, int requiredMilkAmount) {
+        this(requiredGrainsAmount, requiredWaterAmount);
+        this.requiredMilkAmount = requiredMilkAmount;
+    }
+}
